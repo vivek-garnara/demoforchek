6467ce2b1585ca029118127309e43c9352662ec5
jenil ashodia <jenil.ashodia@gmail.com>
2024-07-23 16:56:59 +0530

    commit2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using demoforcheck.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace demoforcheck.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
